e1a674e20de33550f6840dad92a0597570aa7746
Adrian <adriandumitrascu.ro@gmail.com>
2023-07-02 23:44:31 +0200

    init the auth

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js'
-import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js'
+import { getDatabase, ref, push, onValue } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js'
+import { getAuth, connectAuthEmulator, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js'
+
 import data from '/data.js'
 
 const app = initializeApp(data)
 const database = getDatabase(app)
+const shopItems = ref(database, 'testoasa')
+const auth = getAuth(app)
 
-const shopItems = ref(database, 'items')
 
-const inputFieldEl = document.getElementById('ip_field')
+
+const inputFieldEl = document.getElementById('input_field')
 const addBtnEl = document.getElementById('add_btn')
+const shoppingList = document.getElementById('shopping_list')
 
 
-addBtnEl.addEventListener('click', e => {
-  let inputValue = inputFieldEl.value
-  push(shopItems, inputValue)
-  console.log(inputValue)
+addEventListener('click', render)
+addEventListener('keypress', render)
+
+function render(e) {
+  if(e.target.id === "add_btn" || e.key === 'Enter') {
+    let inputValue = inputFieldEl.value
+    push(shopItems, inputValue)
+    inputFieldEl.value = ''
+  }
+}
+
+onValue(shopItems, function(snapshot) {
+  
+  shoppingList.innerHTML = ''
+  let itemsArray = Object.entries(snapshot.val())
+  for(let item in itemsArray){
+    let curentItem = itemsArray[item]
+    let curentItemId = curentItem[0]
+    let curentItemValue = curentItem[1]
+    getHtml(curentItemValue)
+  }
+
 })
 
-// console.log(data)
+function getHtml(item) {
+  shoppingList.innerHTML += `
+      <li>
+        ${item}
+      </li>
+    `
+}
